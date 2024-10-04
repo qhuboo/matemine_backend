5d849c153b1994304e581aa936d735c6a30032c6
Jose Lopez Renteria <joselukas24@gmail.com>
2024-10-03 20:38:23 -0500

    changed registration route to post

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/register", (req, res) => {
+router.post("/register", (req, res) => {
   res.send("Hello");
 });
 
