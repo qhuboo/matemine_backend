b4e2f597c5d5e27823eff75ba8a470bee4e7ddb7
Jose Lopez Renteria <joselukas24@gmail.com>
2024-10-26 18:24:42 -0500

    changed the method for the users register route to test somethign

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-router.post("/register", (req, res) => {
+router.get("/register", (req, res) => {
   res.send("Hello");
 });
 
