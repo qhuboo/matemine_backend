7146a958a64fe120ce86378e45d2dfe78bc16e21
Jose Lopez Renteria <joselukas24@gmail.com>
2024-11-14 21:33:11 -0600

    testing the query string support

--- routes/gameRoutes.js
+++ routes/gameRoutes.js
@@ -5,6 +5,7 @@ const games = require("../db/games/gameDBFunctions");
 
 router.get("/", async (req, res) => {
   try {
+    console.log(req.query);
     const response = await games.getAllGames();
     res.json(response);
   } catch (error) {
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,11 +5,24 @@ router.post("/register", (req, res) => {
   res.send("Hello");
 });
 
-router.post("/signin", (req, res) => {
+router.post("/login", (req, res) => {
   console.log(req.body.email);
   console.log(req.body.password);
 
-  res.json({ message: "the request made it" });
+  // Some sql logic to check password hash and email
+
+  const error = false;
+  // const error = true;
+  if (!error) {
+    // Success Status
+    res.status(201).json({ message: "Successful login" });
+  } else {
+    // Error Status
+    res.status(500).json({
+      message: "This is the success message",
+      error: "This is the error message",
+    });
+  }
 });
 
 module.exports = router;
