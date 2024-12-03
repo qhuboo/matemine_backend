96f16f3f0ebee1d231ee2a1df82f31a9a4534382
Jose Lopez Renteria <joselukas24@gmail.com>
2024-12-02 19:06:40 -0600

    added the jsonwebtoken library and created the generateTokens function

--- utils.js
+++ utils.js
@@ -1,3 +1,6 @@
+const jwt = require("jsonwebtoken");
+const config = require("./config");
+
 function asyncWrapper(handlerFunction) {
   return async function (req, res, next) {
     try {
@@ -8,4 +11,31 @@ function asyncWrapper(handlerFunction) {
   };
 }
 
-module.exports = { asyncWrapper };
+function generateTokens(user) {
+  const accessToken = jwt.sign(
+    {
+      userId: user.user_id,
+      email: user.email,
+      tokenVersion: user.token_version,
+    },
+    config.accessTokenSecret,
+    {
+      expiresIn: config.accessTokenExpiry,
+    }
+  );
+
+  const refreshToken = jwt.sign(
+    {
+      userId: user.user_id,
+      tokenVersion: user.token_version,
+    },
+    config.refreshTokenSecret,
+    {
+      expiresIn: config.refreshTokenExpiry,
+    }
+  );
+
+  return { accessToken, refreshToken };
+}
+
+module.exports = { asyncWrapper, generateTokens };
